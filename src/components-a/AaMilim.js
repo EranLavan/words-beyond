d1a420dfa24da544bf09e3b4a488a8bdcd6bad76
Grigorii Belov <belovgrigory139@gmail.com>
2024-05-04 22:30:03 +0300

    new file:   src/components-a/AaMilim.js

---
+++ src/components-a/AaMilim.js
@@ -0,0 +1,47 @@
+import React, {useState, useRef, useEffect} from 'react';
+//import './Milim.css';
+//import words from '../data/words.js'
+
+let alreadyUsedWords = [];
+let incorrectAnswers = [];
+
+let randomNumber = (Math.ceil(Math.random()*(words.length - 1)));
+
+function getRandomUniqueNumber() {
+  do {
+    randomNumber = (Math.ceil(Math.random()*(words.length - 1)));
+  } while (alreadyUsedWords.includes(randomNumber));
+  return randomNumber;
+}
+
+const randomUniqueNumber = getRandomUniqueNumber();
+
+alreadyUsedWords.push(randomUniqueNumber);
+
+function AaMilim() {
+
+const [inputValue, setInputValue] = useState('');
+const [language, setLanguage] = useState('english');
+const [score, setScore] = useState(0);
+const [word, setWord] = useState(randomUniqueNumber - 1);
+const [counter, setCounter] = useState(1);
+const [message, setMessage] = useState('');
+const [showPronunciation, setShowPronunciation] = useState(false);
+const [showFinalResults, setShowFinalResults] = useState(false);
+const buttonRef = useRef(null);
+const inputRef = useRef(null);
+
+useEffect(() => {
+  inputRef.current.focus();
+}, []);
+
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default AaMilim
